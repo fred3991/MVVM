82c736b295fc2e3485d6373f790c835a682524a3
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-02-08 16:59:14 +0700

    Шаблон MVVM

---
+++ Tests/MVVMTestc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVVMTestc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
