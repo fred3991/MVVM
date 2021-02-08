112be07ddc72fbc770a9ef2507ae79c4bfc06280
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-02-08 17:53:47 +0700

    Заголовок окна, 59,09

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using MVVM.ViewModel.Base;
+
+
+namespace MVVM.ViewModel
+{
+    internal class MainWindowViewModel : Base.ViewModel
+    {
+        private string _Title;
+
+
+        #region Заголовок окна
+
+        /// <summary>
+        /// Заголовок окна?
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+    }
+}
