8a33e9d0708418adcf994084f212c05fa6cf66b7
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-02-08 17:43:07 +0700

    кокойто диспозе

--- ViewModel/Base/ViewModel.cs
+++ ViewModel/Base/ViewModel.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 
 namespace MVVM.ViewModel.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -24,5 +24,26 @@ namespace MVVM.ViewModel.Base
             OnPropertyChanged(PropertyName);
             return true;
         }
+
+
+        //~ViewModel()
+        //{
+        //    Dispose(false);
+        //}
+
+        public void Dispose() 
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            //Освобождение управляемых ресурсов
+        }
+
     }
+
 }
