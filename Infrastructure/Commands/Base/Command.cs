4b69ec73a4d3dd34741fc61ed014ca58c57209a7
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-02-10 11:45:05 +0700

    Тест элементов WPF в библиотеках

--- Infrastructure/Commands/Base/Command.cs
+++ Infrastructure/Commands/Base/Command.cs
@@ -14,10 +14,7 @@ namespace MVVM.Infrastructure.Commands.Base
             add => CommandManager.RequerySuggested += value;
             remove => CommandManager.RequerySuggested -= value;
         }
-
         public abstract bool CanExecute(object parameter);
-
-
         public abstract void Execute(object parameter);
        
     }
