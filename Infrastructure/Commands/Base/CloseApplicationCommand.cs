3eb478cab9928ad45a74e2570feee287affd3092
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-02-10 13:27:29 +0700

    Определение класса отдельной команды

---
+++ Infrastructure/Commands/Base/CloseApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM.Infrastructure.Commands.Base
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+     
+    }
+}
